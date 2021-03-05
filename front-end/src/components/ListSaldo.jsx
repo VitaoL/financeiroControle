d3f14ab195dd2973931a6d3565efeb5b2a7d0b9a
Victor L <victor.lustosacp@gmail.com>
2021-03-05 20:15:22 -0300

    data

--- front-end/src/components/ListSaldo.jsx
+++ front-end/src/components/ListSaldo.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { getTransitions } from '../services/FinanciasApi.js';
+import dateFormat from 'dateformat';
 // import  {moment} from 'moment';
 
 const ListSaldo = (props) => {
+  function dataFormatada(data) {
+    return dateFormat(data, "dd/mm/yyyy");
+  }
   const [array, setArray] = useState([]);
   const [saldo, setSaldo] = useState(0);
   const { name, id } = JSON.parse(localStorage.getItem('userFinancias'));
   useEffect(() => {
     getTransitions(id).then(({ data }) => {
-      console.log(data.allTransfer.createdAt);
       setArray(data.allTransfer);
-      return setSaldo(data.totalSaldo);
+      setSaldo(data.totalSaldo);
     });
   }, [props, id]);
   return (
@@ -18,10 +21,11 @@ const ListSaldo = (props) => {
       <div className="header-account">
         <p>{name}</p>
         <p>
-          {saldo.toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          })}{' '}
+          {saldo &&
+            saldo.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            })}{' '}
         </p>
       </div>
       {array && array.length > 0 && (
@@ -39,9 +43,7 @@ const ListSaldo = (props) => {
                         })}
                       </p>
                       <div className="trans">
-                        <span>
-                          {createdAt}
-                        </span>
+                        <span>{dataFormatada(createdAt)}</span>
                         <span>{description}</span>
                       </div>
                     </div>
@@ -55,7 +57,7 @@ const ListSaldo = (props) => {
                         })}
                       </p>
                       <div className="trans">
-                        <span>24/04/21</span>
+                        <span>{dataFormatada(createdAt)}</span>
                         <span>{description}</span>
                       </div>
                     </div>
